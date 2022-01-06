bd6b67bfd7a8dbac56082c530fb5b6af63398c88
Elshad Usubov <92145698+Usubov-code@users.noreply.github.com>
2022-01-06 14:00:41 +0400

    Add project files.

---
+++ PracticeCrudSimple/Areas/admin/Controllers/EmployeeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PracticeCrudSimple.Data;
+using PracticeCrudSimple.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PracticeCrudSimple.Areas.admin.Controllers
+{
+    [Area("admin")]
+    public class EmployeeController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public EmployeeController(AppDbContext context)
+        {
+            _context = context;
+
+        }
+        public IActionResult Index()
+        {
+
+            return View(_context.Employees.Include(e=>e.Position).ToList());
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Employee model)
+        {
+
+            if (ModelState.IsValid)
+            {
+                _context.Employees.Add(model);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
+        }
+
+    }
+}
---
+++ PracticeCrudSimple/Models/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PracticeCrudSimple.Models
+{
+    public class Employee
+    {
+
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(150)]
+        public string Email { get; set; }
+        [MaxLength(150)]
+        public string FullName { get; set; }
+        
+        public Byte Age { get; set; }
+        [ForeignKey("Position")]
+        public int PositionId { get; set; }
+        public Position   Position { get; set; }
+    }
+}
